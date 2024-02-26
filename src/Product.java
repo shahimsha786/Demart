d31e246827be2764d14d4b1c03642593cf0038ce
shahimsha786 <121596739+shahimsha786@users.noreply.github.com>
2024-02-26 08:37:43 +0530

    myfisrt commit

---
+++ src/Product.java
@@ -0,0 +1,20 @@
+
+public class Product {
+	private String name;
+	private String sku;
+	private int id;
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Product pr=new Product();
+		pr.id=786;
+		pr.name="shirt";
+		pr.sku="lenein-sleeve";
+		
+		
+		
+	}
+
+}
